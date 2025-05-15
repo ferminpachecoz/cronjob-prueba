c71e322ccdd056e9530b0c9a4293b54e4df97418
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2025-05-15 14:47:24 -0300

    more options in driver

--- scrapper_dia2.py
+++ scrapper_dia2.py
@@ -26,8 +26,13 @@ def get_productos():
     options = Options()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
     options.add_argument("--headless=new")  # activar en Railway
-
+    options.add_argument("--window-size=1920,1080")
+    
+    
+    
     driver = webdriver.Chrome(options=options)
 
     total_paginas = 3
