53d50a03cf77e1da77be1fb6c93109be12507c37
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2025-05-14 23:24:18 -0300

    chrom driver static

--- scraper_bonprix.py
+++ scraper_bonprix.py
@@ -12,7 +12,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
-import time
 import re
 
 CHROME_PATH = r"C:\ChromeForTesting\chrome.exe"
